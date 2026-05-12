1b6dbf4acdf4f3da274f5ac568eb23f3574b8980
B-oviedo <bnjmnvd@gmail.com>
2026-05-12 16:21:49 -0400

    Se arreglo diseños y se dio ruta a links

--- src/app/components/content-login/content-login.ts
+++ src/app/components/content-login/content-login.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-content-login',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './content-login.html',
   styleUrl: './content-login.css',
 })
--- src/app/components/content-registro/content-registro.ts
+++ src/app/components/content-registro/content-registro.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-content-registro',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './content-registro.html',
   styleUrl: './content-registro.css',
 })
