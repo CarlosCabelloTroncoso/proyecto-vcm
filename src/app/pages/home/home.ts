22b4b8054f6babdea2cf34272cf03e23bed5dab0
Carlos Cabello Troncoso <c.cabello.troncoso2003@gmail.com>
2026-05-12 22:27:30 -0400

    feat: estructura base y vistas principales UCM Vinculación

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Navbar } from '../../components/navbar/navbar';
+import { Footer } from '../../components/footer/footer';
 import { ContentHome } from '../../components/content-home/content-home';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ContentHome],
+  imports: [CommonModule, Navbar, Footer, ContentHome],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
